0c0d1e062fd6e87ecb3d0ff029529915363b70ea
Jason Harris <jth@mibot.com>
2015-01-25 10:08:18 -0800

    Added test gcode for xyz axes.

--- convert/convert.py
+++ convert/convert.py
@@ -22,7 +22,7 @@ import ply.lex as lex
 tokens = (
   'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
   'INTEGER',
-  'DECIMAL',
+  'FLOAT',
   'COMMENT',
   'NEWLINE',
 )
@@ -55,7 +55,7 @@ t_Y = r'Y|y'
 t_Z = r'Z|z'
 
 t_INTEGER = r'\d+'
-t_DECIMAL = r'[+-]*\d*\.\d*'
+t_FLOAT = r'[+-]*\d*\.\d*'
 
 def t_NEWLINE(t):
   r'\r\n|\n'
@@ -118,6 +118,7 @@ def p_word(p):
           | arc_radius
           | feed_increment
           | dwell_time
+          | subroutine
           | x_offset
           | y_offset
           | z_offset
@@ -180,12 +181,19 @@ def p_misc_function(p):
     p[0] = 'm%s' % p[2]
 
 def p_feed_rate(p):
-  'feed_rate : F DECIMAL'
-  p[0] = 'f%s' % p[2]
+  '''feed_rate : F FLOAT
+               | F INTEGER'''
+  if _comments:
+    p[0] = 'f%s (feed rate)' % p[2]
+  else:
+    p[0] = 'f%s' % p[2]
 
 def p_spindle_speed(p):
   'spindle_speed : S INTEGER'
-  p[0] = 's%s' % p[2]
+  if _comments:
+    p[0] = 's%s (spindle speeed)' % p[2]
+  else:
+    p[0] = 's%s' % p[2]
 
 def p_tool_length_offset_index(p):
   'tool_length_offset_index : H INTEGER'
@@ -195,68 +203,91 @@ def p_tool_length_offset_index(p):
     p[0] = 'h%s' % p[2]
 
 def p_x_axis(p):
-  'x_axis : X DECIMAL'
+  '''x_axis : X FLOAT
+            | X INTEGER'''
   p[0] = 'x%s' % p[2]
 
 def p_y_axis(p):
-  'y_axis : Y DECIMAL'
+  '''y_axis : Y FLOAT
+            | Y INTEGER'''
   p[0] = 'y%s' % p[2]
 
 def p_z_axis(p):
-  'z_axis : Z DECIMAL'
+  '''z_axis : Z FLOAT
+            | Z INTEGER'''
   p[0] = 'z%s' % p[2]
 
 def p_a_axis(p):
-  'a_axis : A DECIMAL'
+  '''a_axis : A FLOAT
+            | A INTEGER'''
   p[0] = 'a%s' % p[2]
 
 def p_b_axis(p):
-  'b_axis : B DECIMAL'
+  '''b_axis : B FLOAT
+            | B INTEGER'''
   p[0] = 'b%s' % p[2]
 
 def p_c_axis(p):
-  'c_axis : C DECIMAL'
+  '''c_axis : C FLOAT
+            | C INTEGER'''
   p[0] = 'c%s' % p[2]
 
 def p_u_axis(p):
-  'u_axis : U DECIMAL'
+  '''u_axis : U FLOAT
+            | U INTEGER'''
   p[0] = 'u%s' % p[2]
 
 def p_v_axis(p):
-  'v_axis : V DECIMAL'
+  '''v_axis : V FLOAT
+            | V INTEGER'''
   p[0] = 'v%s' % p[2]
 
 def p_w_axis(p):
-  'w_axis : W DECIMAL'
+  '''w_axis : W FLOAT
+            | W INTEGER'''
   p[0] = 'w%s' % p[2]
 
 def p_x_offset(p):
-  'x_offset : I DECIMAL'
+  '''x_offset : I FLOAT
+              | I INTEGER'''
   p[0] = 'i%s' % p[2]
 
 def p_y_offset(p):
-  'y_offset : J DECIMAL'
+  '''y_offset : J FLOAT
+              | J INTEGER'''
   p[0] = 'j%s' % p[2]
 
 def p_z_offset(p):
-  'z_offset : K DECIMAL'
+  '''z_offset : K FLOAT
+              | K INTEGER'''
   p[0] = 'k%s' % p[2]
 
 def p_dwell_time(p):
-  'dwell_time : P DECIMAL'
-  p[0] = 'p%s' % p[2]
+  '''dwell_time : P FLOAT
+                | P INTEGER'''
+  if _comments:
+    p[0] = 'p%s (dwell time)' % p[2]
+  else:
+    p[0] = 'p%s' % p[2]
+
+def p_subroutine(p):
+  'subroutine : O INTEGER'
+  if _comments:
+    p[0] = 'o%s (subroutine)' % p[2]
+  else:
+    p[0] = 'o%s' % p[2]
 
 def p_feed_increment(p):
-  'feed_increment : Q DECIMAL'
+  'feed_increment : Q FLOAT'
   p[0] = 'q%s' % p[2]
 
 def p_arc_radius(p):
-  'arc_radius : R DECIMAL'
+  'arc_radius : R FLOAT'
   p[0] = 'r%s' % p[2]
 
 # Error rule for syntax errors
 def p_error(p):
-  print "syntax error at token", p.type
+  print('syntax error at token %s' % p.value)
   # Just discard the token and tell the parser it's okay.
   parser.errok()
 
---
+++ test/fast_test.gcode
@@ -0,0 +1,19 @@
+f400 (feed rate)
+m98 (?) p1 (dwell time)
+m47 (?)
+m30 (program end)
+o1 (subroutine)
+g01 (linear move) y30 x25
+g01 (linear move) z2
+g01 (linear move) z5
+g01 (linear move) y30 x0
+g01 (linear move) z2
+g01 (linear move) z5
+g01 (linear move) x0 y0
+g01 (linear move) z2
+g01 (linear move) z5
+g01 (linear move) y0 x25
+g01 (linear move) z2
+g01 (linear move) z5
+g01 (linear move) y30 x25
+m99 (?)
